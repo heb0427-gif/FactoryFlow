01884d4bc5a6569bf965652f67f5a8bb6223ee83
EunBin Heo <heb0427@gmail.com>
2026-07-27 21:02:36 +0900

    fead: add equipment registration and status menu

--- FactoryFlow/EquipmentMenu.cpp
+++ FactoryFlow/EquipmentMenu.cpp
@@ -50,3 +50,83 @@ void EquipmentMenu::handleDisplayEquipment() const {
 	printEquipment(*equipmentPtr); // 주소가 가리키는 실제 객체를 얻음(역참조)
 }
 
+void EquipmentMenu::handleRegisterEquipment() {
+	string registerID, registerName;
+	cout << "[Register Equipment]\n\n";
+
+	cout << "Equipment ID: ";
+	cin >> registerID;
+
+	cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+	cout << "Equipment Name: ";
+	getline(cin, registerName);
+
+	if (!system.registerEquipment(registerID, registerName)) {
+		cout << "Unable to register the equipment. Please check the ID and name.\n";
+		return;
+	}
+	cout << "Equipment registered.\n";
+}
+
+bool EquipmentMenu::convertMenuNumberToStatus(int menuNumber, EquipmentStatus& status) const {
+	switch (menuNumber) {
+	case 1:
+		status = EquipmentStatus::STOPPED;
+		return true;
+	case 2:
+		status = EquipmentStatus::RUNNING;
+		return true;
+	case 3:
+		status = EquipmentStatus::PAUSED;
+		return true;
+	case 4:
+		status = EquipmentStatus::ERROR;
+		return true;
+	default:
+		return false;
+	}
+}
+
+void EquipmentMenu::handleChangeEquipmentStatus() {
+	string id;
+	int statusNumberToChange;
+
+	cout << "[Change Equipment Status]\n\n";
+
+	cout << "Equipment ID: ";
+	cin >> id;
+
+	const Equipment* equipment = system.findEquipment(id);
+	if (equipment == nullptr) {
+		cout << "Equipment not found.";
+		return;
+	}
+
+	cout << "\n\n";
+
+	cout << "Current Status: " << equipmentStatusToString(equipment->getStatus()) << "\n\n";
+
+	cout << "1. STOPPED\n";
+	cout << "2. RUNNING\n";
+	cout << "3. PAUSED\n";
+	cout << "4. ERROR\n\n";
+
+	cout << "Status to change: ";
+	cin >> statusNumberToChange;
+
+	EquipmentStatus nextStatus;
+
+	if (!convertMenuNumberToStatus(statusNumberToChange, nextStatus)) {
+		cout << "Invalid status number\n";
+		return;
+	}
+
+	if (!system.changeEquipmentStatus(id, nextStatus)) {
+		cout << "Invalid status transition!\n";
+		return;
+	}
+
+	cout << "Equipment status changed to " << equipmentStatusToString(nextStatus) << "\n";
+}
+
